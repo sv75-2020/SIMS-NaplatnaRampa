8a7779a2b4a5c8a004a392b57ec67738195a425f
masha1312 <zorica.pesic01@gmail.com>
2022-06-27 01:09:31 +0200

    dodali izvestaje i cenovnik

--- NaplatnaRampa/NaplatnaRampa/Forme/SefForm.Designer.cs
+++ NaplatnaRampa/NaplatnaRampa/Forme/SefForm.Designer.cs
@@ -29,12 +29,92 @@ namespace NaplatnaRampa.Forme
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.zarada_stanica_btn = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.izvestaj = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // zarada_stanica_btn
+            // 
+            this.zarada_stanica_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zarada_stanica_btn.Location = new System.Drawing.Point(70, 176);
+            this.zarada_stanica_btn.Name = "zarada_stanica_btn";
+            this.zarada_stanica_btn.Size = new System.Drawing.Size(208, 72);
+            this.zarada_stanica_btn.TabIndex = 0;
+            this.zarada_stanica_btn.Text = "Zarada na stanici";
+            this.zarada_stanica_btn.UseVisualStyleBackColor = true;
+            this.zarada_stanica_btn.Click += new System.EventHandler(this.zarada_stanica_btn_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(18, 52);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(256, 52);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Od:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(256, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Do:";
+            // 
+            // izvestaj
+            // 
+            this.izvestaj.AutoSize = true;
+            this.izvestaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.izvestaj.Location = new System.Drawing.Point(65, 113);
+            this.izvestaj.Name = "izvestaj";
+            this.izvestaj.Size = new System.Drawing.Size(0, 29);
+            this.izvestaj.TabIndex = 5;
+            // 
+            // SefForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1158, 663);
+            this.Controls.Add(this.izvestaj);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.zarada_stanica_btn);
+            this.Name = "SefForm";
             this.Text = "SefForm";
+            this.Load += new System.EventHandler(this.SefForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button zarada_stanica_btn;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label izvestaj;
     }
 }
---
+++ NaplatnaRampa/NaplatnaRampa/Klase/Cenovnik.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaplatnaRampa.Klase
+{
+    public class Cenovnik
+    {
+        private DateTime pocetak;
+        private DateTime kraj;
+        private float cenaPoKm;
+        private Dictionary<TipVozila, float> cenaPoTipu = new Dictionary<TipVozila, float>();
+
+        public Cenovnik(DateTime pocetak,DateTime kraj,float cena)
+        {
+            this.pocetak = pocetak;
+            this.kraj = kraj;
+            this.cenaPoKm = cena;
+        }
+
+        public void LoadCenaPoTipu(string[] vrednosti)
+        {
+            int i = 0;
+            foreach (TipVozila tip in (TipVozila[])Enum.GetValues(typeof(TipVozila)))
+            {
+                cenaPoTipu.Add(tip, float.Parse(vrednosti[i]));
+                i++;
+            }
+        }
+
+        public DateTime Pocetak()
+        {
+            return pocetak;
+        }
+        public DateTime Kraj()
+        {
+            return kraj;
+        }
+
+        public float CenaPoKm()
+        {
+            return cenaPoKm;
+        }
+        public Dictionary<TipVozila, float> CenaPoTipu() {
+            return cenaPoTipu;
+        }
+    }
+}
